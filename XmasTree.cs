8328ba8407a3c5a8a6db5cc7efb7959ee303b776
Ivan <ivan.naumov92@yahoo.com>
2018-06-24 16:34:32 +0300

    Import

---
+++ XmasTree.cs
@@ -0,0 +1,26 @@
+﻿husing System;
+
+namespace ChristmasTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            GetChristmasTree(size);
+        }
+
+        static void GetChristmasTree(int number)
+        {
+            int stars = 0;
+            int spaces = number;
+
+            for (int i = 0; i <= number; i++)
+            {
+                stars = i;
+                Console.WriteLine("{1}{0} | {0}{1}", new string('*', stars), new string(' ', spaces));
+                spaces--;
+            }
+        }
+    }
+}
